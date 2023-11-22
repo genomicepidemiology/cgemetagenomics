d4c9c7d59a5e22b2d16db08b87129e3614a250a6
MBHallgren <s153002@student.dtu.dk>
2023-11-22 14:29:35 +0100

    meta19

--- cgemetagenomics/metagenomics_pipeline.py
+++ cgemetagenomics/metagenomics_pipeline.py
@@ -33,7 +33,7 @@ def metagenomics_pipeline(args):
 
     amr_results = read_tab_separated_file(args.output + "/amr.res")
 
-    report = create_refined_report(args.db_dir + '/phenotypes.txt', args.output, bacterial_results)
+    report = create_refined_report(args.db_dir + '/phenotypes.txt', args.output, bacterial_results, species)
     print (report)
 
     #Parse bacterial alignment and output those above a set of thresholds
