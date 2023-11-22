9884460265a3915262d804cffceecd8962215b63
MBHallgren <s153002@student.dtu.dk>
2023-11-22 12:39:52 +0100

    meta4

--- cgemetagenomics/metagenomics_pipeline.py
+++ cgemetagenomics/metagenomics_pipeline.py
@@ -8,17 +8,33 @@ from cgemetagenomics import kma
 def metagenomics_pipeline(args):
     os.system('mkdir ' + args.output)
     # Check if kma is installed
+
+    load_pathogen_strains(args.db_dir + '/pathogen_strains.list')
+    sys.exit()
     kma.KMARunner(args.input,
               args.output + "/bacteria_alignment",
               args.db_dir + '/bac_db/bac_db',
               "-ID 25 -md 1 -ont -1t1 -mem_mode -t 8").run()
 
     baterial_results = read_tab_separated_file(args.output + "/bacteria_alignment.res")
-    for item in baterial_results:
-        print (item)
+
+    kma.KMARunner(args.input,
+                  args.output + "/amr",
+                  args.db_dir + '/resfinder_db/resfinder_db',
+                  "-ont -md 3 -mem_mode -t 8").run()
+
+    amr_results = read_tab_separated_file(args.output + "/amr.res")
+
     #Parse bacterial alignment and output those above a set of thresholds
 
     return 'isolate_pipeline'
+
+def load_pathogen_strains(strain_file):
+    strains = []
+    with open(strain_file, 'r') as file:
+        for line in f:
+            line = line.strip().split('\t')
+            print (line)
 
 def read_tab_separated_file(file_path):
     results = []
