6d64a4fdcfd056115c1d264bbbb721e1a73e0789
MBHallgren <s153002@student.dtu.dk>
2023-11-22 12:29:01 +0100

    meta3

--- cgemetagenomics/metagenomics_pipeline.py
+++ cgemetagenomics/metagenomics_pipeline.py
@@ -13,18 +13,17 @@ def metagenomics_pipeline(args):
               args.db_dir + '/bac_db/bac_db',
               "-ID 25 -md 1 -ont -1t1 -mem_mode -t 8").run()
 
-    baterial_results = parse_bacterial_alignment_results(args.output + "/bacteria_alignment.res")
+    baterial_results = read_tab_separated_file(args.output + "/bacteria_alignment.res")
     for item in baterial_results:
         print (item)
     #Parse bacterial alignment and output those above a set of thresholds
 
     return 'isolate_pipeline'
 
-def parse_bacterial_alignment_results(res_file):
-    def read_tab_separated_file(file_path):
-        results = []
-        with open(file_path, 'r') as file:
-            reader = csv.DictReader(file, delimiter='\t')
-            for row in reader:
-                results.append(row)
-        return results
+def read_tab_separated_file(file_path):
+    results = []
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file, delimiter='\t')
+        for row in reader:
+            results.append(row)
+    return results
